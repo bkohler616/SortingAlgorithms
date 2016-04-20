e0fd6e9bcbe946c0e57fa84de4229c94e43486a2
Benjamin Kohler <bkohler616@hotmail.com>
2016-04-20 18:33:47 -0400

    Initial commit. Added SortingBase for individual sorts.

---
+++ SortAlgos/SortAlgos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAlgos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nothing here yet.
+        }
+    }
+}
---
+++ SortAlgos/SortAlgos/SortingBase.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace SortAlgos
+{
+    public abstract class SortingBase
+    {
+        /// <summary>
+        /// The size limit of the data list.
+        /// </summary>
+        private const int MaxSize = 500000;
+        /// <summary>
+        /// The data where all of the sorting information will be stored.
+        /// </summary>
+        public int[] SortingList { get; set; }
+
+        /// <summary>
+        /// Generate descending data
+        /// </summary>
+        public void GenerateDescendingData()
+        {
+            SortingList = new int[MaxSize];
+            int temp = MaxSize;
+            for (int i = 0; temp > 0; temp--)
+            {
+                SortingList[i] = temp;
+                i++;
+            }
+        }
+
+        /// <summary>
+        /// Generate random integers to be sorted.
+        /// </summary>
+        public void GenerateRandomData()
+        {
+            var rng = new Random();
+            SortingList = new int[MaxSize];
+            //Generate numbers
+            for (var i = 0; i < MaxSize; i++)
+                SortingList[i] = rng.Next(MaxSize);
+        }
+
+        /// <summary>
+        /// Generate Ascending integers to be sorted
+        /// </summary>
+        public void GenerateAscendingData()
+        {
+            SortingList = new int[MaxSize];
+            for (var i = 0; i < MaxSize; i++)
+                SortingList[i] = i;
+        }
+
+        /// <summary>
+        /// Begin sorting the SortingList data.
+        /// </summary>
+        public void PerformSort()
+        {
+            throw new NotImplementedException("The sort does not have any procedures.\n" +
+                                              "Please implement an override for the PreformSort function");
+        }
+
+        /// <summary>
+        /// Output the SortingList data to file
+        /// </summary>
+        /// <param name="fileName">The file name to output to (do not add .txt)</param>
+        public void OutputListToFile(string fileName)
+        {
+            fileName = fileName + ".txt";
+            var file = new StreamWriter(fileName);
+            foreach (var i in SortingList)
+            {
+                file.WriteLine(i);
+            }
+            Process.Start(fileName);
+        }
+    }
+}
