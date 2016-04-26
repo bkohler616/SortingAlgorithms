8af55909a9939622937545e090afc588f47e5ad2
Benjamin Kohler <bkohler616@hotmail.com>
2016-04-26 11:46:16 -0400

    Made quicksort create only 2 child threads on the first run. One for the lows, one for the highs.

--- SortAlgos/SortAlgos/QuickSort.cs
+++ SortAlgos/SortAlgos/QuickSort.cs
@@ -1,68 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+
 
 namespace SortAlgos
 {
-    class QuickSort : SortingBase
+    public class QuickSort : SortingBase
     {
-        public override void PerformSort()
-        {
-            quickSort(SortingList, 0, SortingList.Length - 1);
-        }
+        public override void PerformSort() { quickSort(SortingList, 0, SortingList.Length - 1, true); }
 
         /// <summary>
-        /// Quicksort performed via a middle-item type
+        ///     Quicksort performed via a middle-item type
         /// </summary>
         /// <param name="data">the data to sort</param>
         /// <param name="low">the low end of the current pivot</param>
         /// <param name="high">the high end of the current pivot</param>
-        private void quickSort(int[] data, int low, int high)
-        {
-
+        private void quickSort(int[] data, int low, int high, bool isMultithread) {
             //No more sorting to do for this pivot end.
             if (low >= high)
                 return;
 
             //Get the middle item
-            int middle = low + (high - low) / 2;
-            int pivot = data[middle];
+            var middle = low + (high - low)/2;
+            var pivot = data [middle];
 
             int incrementalLow = low, incrementalHigh = high;
-            while (incrementalLow <= incrementalHigh)
-            {
+            while (incrementalLow <= incrementalHigh) {
                 //Keep incrementing the low until we find an item >= the middle data
-                while (data[incrementalLow] < pivot)
-                {
+                while (data [incrementalLow] < pivot) {
                     incrementalLow++;
                 }
                 //Keep decrementing the high until we find an item <= the middle item
-                while (data[incrementalHigh] > pivot)
-                {
+                while (data [incrementalHigh] > pivot) {
                     incrementalHigh--;
                 }
 
                 //If we managed to get low to get past high, assume it's sorted properly.
-                if (incrementalLow <= incrementalHigh)
-                {
+                if (incrementalLow <= incrementalHigh) {
                     //Swap data items
-                    int temp = data[incrementalLow];
-                    data[incrementalLow] = data[incrementalHigh];
-                    data[incrementalHigh] = temp;
+                    var temp = data [incrementalLow];
+                    data [incrementalLow] = data [incrementalHigh];
+                    data [incrementalHigh] = temp;
                     incrementalLow++;
                     incrementalHigh--;
                 }
             }
 
             //Create new threads, and go further on each pivot if needed.
-            var sortTasks = new List<Task>();
-            if (low < incrementalHigh)
-                sortTasks.Add(Task.Factory.StartNew(() => { quickSort(data, low, incrementalHigh); }));
-            if (high > incrementalLow)
-                sortTasks.Add(Task.Factory.StartNew(() => { quickSort(data, incrementalLow, high); }));
-            Task.WaitAll(sortTasks.ToArray());
+            if (isMultithread) {
+                if (low < incrementalHigh) {
+                    Task.Factory.StartNew(() => { quickSort(data, low, incrementalHigh, false); });
+                }
+                if (high > incrementalLow) {
+                    Task.Factory.StartNew(() => { quickSort(data, incrementalLow, high, false); });
+                }
+                //Task.WaitAll(lowEnd, highEnd);//Aparently, this adds 300 ms to the time.
+            }
+            else {
+                if (low < incrementalHigh)
+                    quickSort(data, low, incrementalHigh, false);
+                if (high > incrementalLow)
+                    quickSort(data, incrementalLow, high, false);
+            }
         }
     }
 }
